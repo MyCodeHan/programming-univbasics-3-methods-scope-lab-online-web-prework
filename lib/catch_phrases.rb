8669687a15204deb6d603bb617d4e52cde736be7
Hannah Harrington <hannahharrington@outlook.com>
2019-08-05 20:42:32 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++
@@ -1,6 +0,0 @@
-describe "any_phrase" do
-  it "takes in an argument and puts out the catch phrase" do
-    phrase = "It's Monday Time for a barrel roll!!"
-    expect{any_phrase (phrase)}.to output("It's Monday Time for a barrel roll!\n").to_stdout
-  end
-end
