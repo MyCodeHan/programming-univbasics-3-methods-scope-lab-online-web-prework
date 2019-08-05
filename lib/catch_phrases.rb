e18edd0c8348130b690521314c3d75fa4d6b4426
Hannah Harrington <hannahharrington@outlook.com>
2019-08-05 20:57:17 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -1,2 +1,2 @@
 def link
-  "puts out'It's Dangerous To Go Alone! Take This."do expect {link}.to_output (It's Dangerous To Go Alone! Take This.\n)
+  "puts out'It's Dangerous To Go Alone! Take This.'" do expect {link}.to output (It's Dangerous To Go Alone! Take This.\n)
